d402bd9e4a0865512185dd37cf86b0cd9533c6a9
Alicia Secord <alicia.m.secord@gmail.com>
2018-09-06 17:46:09 -0400

    Added fetch for APIs
    Fetch APIs for trash and single permits

--- functions/index.js
+++ functions/index.js
@@ -4,15 +4,11 @@
 
 const functions = require('firebase-functions');
 const fetch = require('node-fetch');
-const https = require('https');
 const {WebhookClient} = require('dialogflow-fulfillment');
 const {Card, Suggestion} = require('dialogflow-fulfillment');
 
 
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
-
-const permitshost = 'https://data.detroitmi.gov'
-const trashhost = 'https://apis.detroitmi.gov/waste_notifier/address/'
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
   const agent = new WebhookClient({ request, response });
@@ -32,37 +28,58 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   // Get address from user input
   const address = parameter.short_address;
 
+  // Get the main location for the permits api
+  const permitsurl = `https://data.detroitmi.gov/resource/but4-ky7y.json?$q=`;
+
   // Call the address API
   function permitssingle(agent){
     agent.add(`Looking up permits for ${address}`);
-    singlepermits(address).then((output) => {
-      response.json({ 'fulfillmentMessages': output });// Return the results of the API to Dialogflow
-      return null;
-    }).catch(() => {
-      response.json({ 'fulfillmentMessages': `I don't know` });
-    });
+
+    fetch(permitsurl + encodeURIComponent(address))
+      .then(response => {return response.json()})
+      .then(data => {
+          agent.add(`There is ${data.length} permit for that ${address}.`)
+          return data.length;
+        })
+      .catch(agent.add(`I don't know`))
   }
   // Get trash type from user input
   const trash_type = parameter.trash_type;
 
-  // // Uncomment and edit to make your own intent handler
-  // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
-  // // below to get this function to be run when a Dialogflow intent is matched
-  // function yourFunctionHandler(agent) {
-  //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
-  //   agent.add(new Card({
-  //       title: `Title: this is a card title`,
-  //       imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
-  //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
-  //       buttonText: 'This is a button',
-  //       buttonUrl: 'https://assistant.google.com/'
-  //     })
-  //   );
-  //   agent.add(new Suggestion(`Quick Reply`));
-  //   agent.add(new Suggestion(`Suggestion`));
-  //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
-  // }
+  // Array to define day of the week
+  let weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
+  // Get the location for trash
+  const trashurl = 'https://apis.detroitmi.gov/waste_notifier/address/'
+
+  // Sample for trash
+  function trashlookup(agent) {
+    agent.add(`Searching for ${trash_type} in ${trashhost + address + '/'}`);
+
+    fetch(trashurl + encodeURIComponent(address) + '/')
+      .then(response => {return response.json()})
+      .then(data => {
+        if (trash_type === 'trash'){
+          let date = new Date(data.next_pickups.trash.date)
+        }
+        else if (trash_type === 'bulk'){
+          let date = new Date(data.next_pickups.bulk.date)
+        }
+        else if (trash_type === 'yard waste'){
+          let date = new Date(data.next_pickups['yard waste'].date)
+        }
+        else if (trash_type === 'recycling'){
+          let date = new Date(data.next_pickups.recycling.date)
+        }
+        else
+          {return null}
+
+        let day = weekday[date.getDay()]
+        agent.add(`${trash_type} pickup is on ${day}`)
+        return day
+      })
+      .catch(agent.add(`I'm not sure`));
+    }
 
   // Run the proper function handler based on the matched Dialogflow intent name
   let intentMap = new Map();
@@ -75,35 +92,3 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   agent.handleRequest(intentMap);
 
 });
-
-function singlepermits(address){
-  return new Promise((resolve, reject) => {
-    // Create the path for the HTTP request to get the permit for a single address
-    let path = '/resource/but4-ky7y.json' + '?$q=' + encodeURIComponent(address);
-    console.log('API request: ' + host + path);
-
-    // Make HTTP request to get the permits
-    https.get(permitshost + path, (res) => {
-      let body = ''; // var to store the response chunks
-      res.on('data', (d) => { body += d; }); // store each response chunk
-      res.on('end', () => {
-        // After all the data has been received parse the JSON for desired data
-        let response = JSON.parse(body);
-        let numberofpermits = response.length;
-
-        // Create response
-        if (numberofpermits === 1)
-          {let output = `There is ${numberofpermits} for that address.`}
-        else
-          {let output = `There are ${numberofpermits} for that address.`}
-
-        // Resolve the promise with the output
-        console.log(output);
-        resolve(output);
-      });
-      res.on('error', (error) => {
-        console.log(`Error calling the permits API: ${error}`)
-      });
-    });
-  });
-}
