b135282aea4c3dd2b3c085ad72d824e0234e5061
Alicia Secord <alicia.m.secord@gmail.com>
2018-09-11 09:24:09 -0400

    Rewritten to fix error
    Fixes 'Error: Invalid or unknown request type (not a Dialogflow v1 or v2 webhook request).'
    
    Now shows 'Error: Dialogflow IntentHandler not found for intent: '

--- functions/index.js
+++ functions/index.js
@@ -2,110 +2,101 @@
 // for Dialogflow fulfillment library docs, samples, and to report issues
 'use strict';
 
+// Import the Dialogflow module from the Actions on Google client library.
+const {dialogflow} = require('actions-on-google');
+
+// Import the fetch module
+const axios = require('axios');
+
+// Import the firebase-functions package for deployment.
 const functions = require('firebase-functions');
-const fetch = require('node-fetch');
-const {WebhookClient} = require('dialogflow-fulfillment');
-const {Card, Suggestion} = require('dialogflow-fulfillment');
 
+// Instantiate the Dialogflow client.
+const app = dialogflow({debug: true});
 
-process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+// Default welcome agent
+app.intent('DefaultWelcomeIntent', (conv) => {
+  conv.add(`Welcome to my agent!`);
+})
 
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
-  const agent = new WebhookClient({ request, response });
-  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+// Default fallback agent
+app.intent('DefaultFallbackIntent', (conv) => {
+  conv.add(`I didn't understand. `);
+  conv.add(`I'm sorry, can you try again?`);
+})
 
-  // Make it easier to call parameters
-  const parameter = request.body.queryResult.parameters;
+// Fetch the permits API
+app.intent('permits.single', singlepermitFunction);
 
-  // Default welcome agent
-  function welcome(agent) {
-    agent.add(`Welcome to my agent!`);
-  }
+function singlepermitFunction (conv, {short_address}) {
+  // API url for permits at a single address
+  const permitsurl = `https://data.detroitmi.gov/resource/but4-ky7y.json?$q=` + encodeURIComponent(short_address);
+  // fetch the url
+  axios.get(permitsurl)
+    .then (response => {response.json()})
+    .then(data => {
+      // Return the number of permits at the address and ask the user how to procede
+      if (data.length === 1) {
+        conv.add(`There is 1 permit for ${short_address}.`)
+        conv.add(` Would you like more details about that permit?`)
+      }
+      else {
+        conv.add(`There are ${data.length} permits for ${short_address}.`)
+        conv.add(`Would you like more details about these permits?`)
+      }
+    })
+    // print something if the above doesn't work
+    .catch(
+      conv.close(`It didn't work for ${short_address}`)
+    )
+};
 
-  // Default fallback agent
-  function fallback(agent) {
-    agent.add(`I didn't understand`);
-    agent.add(`I'm sorry, can you try again?`);
-  }
+// Array to define day of the week
+let weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
-  // Get address from user input
-  const address = parameter.short_address;
+// Fetch the trash API
+app.intent('trash', (conv, {short_address, trash_type}) => {
+  // API url for trash address
+  const trashurl = 'https://apis.detroitmi.gov/waste_notifier/address/' + encodeURIComponent(short_address) + '/'
 
-  // This is the url for the permits api
-  const permitsurl = `https://data.detroitmi.gov/resource/but4-ky7y.json?$q=`;
+  // fetch the url
+  axios.get(trashurl)
+    .then(response => {return response.json()})
+    .then(data => {
+      // Return the date for each type of trash
+      if (trash_type === 'trash'){
+        let date = new Date(data.next_pickups.trash.date)
+      }
+      else if (trash_type === 'bulk'){
+        let date = new Date(data.next_pickups.bulk.date)
+      }
+      else if (trash_type === 'yard waste'){
+        let date = new Date(data.next_pickups['yard waste'].date)
+      }
+      else if (trash_type === 'recycling'){
+        let date = new Date(data.next_pickups.recycling.date)
+      }
+      // Not sure what to do with this else statement
+      else
+        {return null}
 
-  // Fetch the permits API
-  function permitssingle(agent){
-    fetch(permitsurl + encodeURIComponent(address))
-      .then(response => {return response.json()})
-      .then(data => {
-          // Return the number of permit records for that address
-          if (data.length === 1)
-            {agent.add(`There is ${data.length} permit for that ${address}.`)}
-          else {agent.add(`There are ${data.length} permits for that ${address}`)}
-          // then statment has to have a return
-          return data.length;
-        })
-      // Print something if the above doesn't work
-      .catch(agent.add(`I don't know`))
-  }
-  // Get trash type from user input
-  const trash_type = parameter.trash_type;
+      // Turn date into day of the week
+      let day = weekday[date.getDay()]
 
-  // Array to define day of the week
-  let weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+      // return day
+      return day
+    })
+    .then(day => {
+      // Use day to create response
+      conv.add(`${trash_type} pickup is on ${day}.`)
+      conv.add(` Would you like to sign up for reminders?`)
 
-  // Get the url for trash API
-  const trashurl = 'https://apis.detroitmi.gov/waste_notifier/address/'
+      // then statment has to have a return
+      return day
+    })
+    // Print something if the above doesn't work
+    .catch(conv.close(`I'm not sure`));
+})
 
-  // Fetch the trash API
-  function trashlookup(agent) {
-    fetch(trashurl + encodeURIComponent(address) + '/')
-      .then(response => {return response.json()})
-      .then(data => {
-        // Return the date for each type of trash
-        if (trash_type === 'trash'){
-          let date = new Date(data.next_pickups.trash.date)
-        }
-        else if (trash_type === 'bulk'){
-          let date = new Date(data.next_pickups.bulk.date)
-        }
-        else if (trash_type === 'yard waste'){
-          let date = new Date(data.next_pickups['yard waste'].date)
-        }
-        else if (trash_type === 'recycling'){
-          let date = new Date(data.next_pickups.recycling.date)
-        }
-        // Not sure what to do with this else statement
-        else
-          {return null}
-
-        // Turn date into day of the week
-        let day = weekday[date.getDay()]
-
-        // return day
-        return day
-      })
-      .then(day => {
-        // Use day to create response
-        agent.add(`${trash_type} pickup is on ${day}`)
-
-        // then statment has to have a return
-        return day
-      })
-      // Print something if the above doesn't work
-      .catch(agent.add(`I'm not sure`));
-    }
-
-  // Run the proper function handler based on the matched Dialogflow intent name
-  let intentMap = new Map();
-  intentMap.set('Default Welcome Intent', welcome);
-  intentMap.set('Default Fallback Intent', fallback);
-  intentMap.set('permits.single', permitssingle)
-  intentMap.set('trash', trashlookup);
-  // intentMap.set('your intent name here', yourFunctionHandler);
-  // intentMap.set('your intent name here', googleAssistantHandler);
-  agent.handleRequest(intentMap);
-
-});
+// Set the DialogflowApp object to handle the HTTPS POST request.
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
