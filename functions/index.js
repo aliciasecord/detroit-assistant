6b46112a74b1c9d94dfb4837875d722385defad4
Alicia Secord <alicia.m.secord@gmail.com>
2018-10-05 23:20:52 -0400

    Added demolitions intent

--- functions/index.js
+++ functions/index.js
@@ -2,101 +2,283 @@
 // for Dialogflow fulfillment library docs, samples, and to report issues
 'use strict';
 
-// Import the Dialogflow module from the Actions on Google client library.
-const {dialogflow} = require('actions-on-google');
+const functions = require('firebase-functions');
+const { WebhookClient } = require('dialogflow-fulfillment');
+const { Card, Suggestion } = require('dialogflow-fulfillment');
+const admin = require('firebase-admin');
 
-// Import the fetch module
+// Networking
 const fetch = require('node-fetch');
 
-// Import the firebase-functions package for deployment.
-const functions = require('firebase-functions');
+// Setup firestore connections
+admin.initializeApp(functions.config().firebase);
+var db = admin.firestore();
 
-// Instantiate the Dialogflow client.
-const app = dialogflow({debug: true});
+// enables lib debugging statements
+process.env.DEBUG = 'dialogflow:debug';
 
-// Default welcome agent
-app.intent('DefaultWelcomeIntent', (conv) => {
-  conv.add(`Welcome to my agent!`);
-})
+const checkUserStatus = () => {
+  /* Check if the current session
+     user already has any saved details
+     on the system
+  */
+}
 
-// Default fallback agent
-app.intent('DefaultFallbackIntent', (conv) => {
-  conv.add(`I didn't understand. `);
-  conv.add(`I'm sorry, can you try again?`);
-})
+const checkIntentSource = (request) => {
+  // Requests from dialog flow web interface do not come with a intent source
+  // At some point we should return the entire user payload for storage
+  const source = request.body.originalDetectIntentRequest.source || "debug"
+  let userId;
+  if (source === "twilio") {
+    userId = request.body.originalDetectIntentRequest.payload.data.From
+  } else if (source === "google") {
+    userId = request.body.originalDetectIntentRequest.payload.user.userId
+  }else{
+    console.log("Debug user")
+    userId = "6435"
+  }
+  return { source, userId}
+}
 
-// Fetch the permits API
-app.intent('permits.single', singlepermitFunction);
-
-function singlepermitFunction (conv, {short_address}) {
-  // API url for permits at a single address
-  const permitsurl = `https://data.detroitmi.gov/resource/but4-ky7y.json?$q=` + encodeURIComponent(short_address);
-  // fetch the url
-  fetch(permitsurl)
-    .then (response => {response.json()})
-    .then(data => {
-      // Return the number of permits at the address and ask the user how to procede
-      if (data.length === 1) {
-        conv.add(`There is 1 permit for ${short_address}.`)
-        conv.add(` Would you like more details about that permit?`)
-      }
-      else {
-        conv.add(`There are ${data.length} permits for ${short_address}.`)
-        conv.add(`Would you like more details about these permits?`)
+// If there is a user, checkUserExistance payload contains the user data for use
+// Or false if no user is found
+// Returns the entire promise so you can access the data for other intents
+const checkUserExistance = (request) => {
+  const user = checkIntentSource(request);
+  var userRef = db.collection('users').doc(`${user.userId}`);
+  var getDoc = userRef.get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log('No such document!');
+        return false
+      } else {
+        console.log('Document data:', doc.data());
+        return doc.data()
       }
     })
-    // print something if the above doesn't work
-    .catch(
-      conv.close(`It didn't work for ${short_address}`)
-    )
-};
+    .catch(err => {
+      console.log('Error getting document', err);
+      return false
+    });
+  return getDoc
+}
 
-// Array to define day of the week
-let weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const createNewUser = (request) => {
+  const newUser = checkIntentSource(request)
+  console.log("User details:", newUser)
+  var docRef = db.collection('users').doc(`${newUser.userId}`);
+  var setUser = docRef.set({
+    userId: newUser.userId
+  });
+  /* Save a new record for the user
+     either a phone number or google assistant code
+  */
+}
 
-// Fetch the trash API
-app.intent('trash', (conv, {short_address, trash_type}) => {
-  // API url for trash address
-  const trashurl = 'https://apis.detroitmi.gov/waste_notifier/address/' + encodeURIComponent(short_address) + '/'
+const getUserDetails = (userId) => {
+  // Return the user details for use in an intent
+  // This is actually covered with checkUserExistance but you may want to do it directly
+  // We may also want to maintain single responsibility
+}
 
-  // fetch the url
-  fetch(trashurl)
-    .then(response => {return response.json()})
-    .then(data => {
-      // Return the date for each type of trash
-      if (trash_type === 'trash'){
-        let date = new Date(data.next_pickups.trash.date)
+const updateUserDetails = (userId, details) => {
+  // Write items to the user file such as phone and email
+}
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+
+  // Default welcome agent
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+
+  // Default fallback agent
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+
+  // Fulfillment for trash intent
+  function trash(agent) {
+    // set variable for entities
+    const params = request.body.queryResult.parameters;
+    const address = params["short_address"];
+    const trash_type = params["trash_type"];
+
+    // API url for trash address
+    const trashurl = 'https://apis.detroitmi.gov/waste_notifier/address/' + encodeURIComponent(address) + '/?format=json'
+
+    // fetch the trash api
+    return fetch(trashurl)
+      .then(response => response.json())
+      .then(data => {
+        // // Return the date for each type of trash
+        const date = new Date(data.next_pickups[trash_type].date)
+        const todays_date = new Date().getDate()
+        const options = { weekday: 'long' };
+        const day = date.toLocaleDateString("en-GB", options)
+
+        // TODO: Check if hours are past 10 and tell the "Next xDay"
+        if (date.getDate() === todays_date) {
+          return agent.add(`Your next ${trash_type} pickup is today.`)
+        } else {
+          return agent.add(`Your next ${trash_type} pickup is ${day}.`)
+        }
+        return agent.add(`Would you like to sign up for reminders?`)
+
+      }).catch(err => {
+        // Print something if the above doesn't work
+        agent.add(`Sorry we're taking a little longer on our side than expected. Please try again soon.`)
+        console.log("Error:", err)
+        return err
+      });
+  }
+
+  function permitsDetails(agent){
+    const propertyContext = agent.getContext('permitstosend').parameters.permits;
+    return agent.add(`So there were ${propertyContext.totalCount} permits in your last request.`)
+  }
+
+  // Fulfillment for single permit intent
+  function permitsSingle(agent){
+    //createNewUser(request)
+    checkUserExistance(request).then(response => console.log("From the promise:",response)).catch(err => console.log(err))
+
+      // set variable for entities
+      const params = request.body.queryResult.parameters;
+      const address = params["short_address"];
+
+      // set the endpoint and query for graphql
+      const gqlEndpoint = `https://detroit-opendata.ngrok.io/graphql`
+      const gqlQuery = `{
+            geocodeAddress(address: "${address}") {
+              edges {
+                node {
+                  parcelno
+                  address
+                  wkbGeometry
+                  permitsByParcelno {
+                    totalCount
+                    edges {
+                      node {
+                        permitNo
+                        bldPermitType
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }`;
+
+      // fetch graphql endopoint
+      return fetch(gqlEndpoint, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/graphql' },
+        body: gqlQuery,
+      })
+        .then(res => res.json())
+        .then(data => {
+          console.log("permit Data", data);
+        let total = data.data.geocodeAddress.edges.map(e =>
+          e.node.permitsByParcelno.totalCount
+        )
+
+        if (total[0] === 0) {
+          return agent.add("This property does not currently have any building permits.")
+        } else if (total[0] > 4) {
+          agent.setContext({
+            name: 'permitstosend',
+            lifespan: 2,
+            parameters: { permits: data.data.geocodeAddress.edges[0].node.permitsByParcelno }
+          })
+          agent.add(`This property has ${total} permits. There are too many to list, would you like this information texted or emailed to you?`)
+          return agent.add(new Suggestion("Yes"))
+        } else {
+
+          const nodes = data.data.geocodeAddress.edges;
+
+          let permitResponses = ""
+          for (let node of nodes) {
+            for (let newNode of node.node.permitsByParcelno.edges) {
+              permitResponses += `Number: ${newNode.node.permitNo}, Type: ${newNode.node.bldPermitType}.`
+            }
+          }
+          agent.add(`This property has ${total} permits. They are as follows:`)
+          return agent.add(permitResponses)
+        }
+
       }
-      else if (trash_type === 'bulk'){
-        let date = new Date(data.next_pickups.bulk.date)
-      }
-      else if (trash_type === 'yard waste'){
-        let date = new Date(data.next_pickups['yard waste'].date)
-      }
-      else if (trash_type === 'recycling'){
-        let date = new Date(data.next_pickups.recycling.date)
-      }
-      // Not sure what to do with this else statement
-      else
-        {return null}
 
-      // Turn date into day of the week
-      let day = weekday[date.getDay()]
+      )
+        .catch(e => console.log(e));
+    }
 
-      // return day
-      return day
-    })
-    .then(day => {
-      // Use day to create response
-      conv.add(`${trash_type} pickup is on ${day}.`)
-      conv.add(` Would you like to sign up for reminders?`)
+    // Function for demolition intent
+    function demolitionSingle(agent) {
+      // set variable for entities
+      const params = request.body.queryResult.parameters;
+      const address = params["short_address"];
 
-      // then statment has to have a return
-      return day
-    })
-    // Print something if the above doesn't work
-    .catch(conv.close(`I'm not sure`));
-})
+      // set graphql endpoint
+      const gqlEndpoint = `https://detroit-opendata.ngrok.io/graphql`
+      const gqlQuery = `{
+            geocodeAddress(address: "${address}") {
+              edges {
+                node {
+                  parcelno
+                  address
+                  wkbGeometry
+                  demosByParcelno {
+                    edges {
+                      node {
+                        demolitionDate
+                        status
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }`;
 
-// Set the DialogflowApp object to handle the HTTPS POST request.
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
+      const today = new Date();
+      var todaysDate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+      // fetch demolitions
+      return fetch(gqlEndpoint, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/graphql' },
+        body: gqlQuery
+      })
+      .then(res => res.json())
+      .then(data => {
+          let demoPlanned = data.data.geocodeAddress.edges[0]
+          if (!demoPlanned){
+            return agent.add(`There are no demolitions planned for ${address}.`)
+          }
+          let demoDate = data.data.geocodeAddress.edges[0].node.demosByParcelno.edges[0].node.demolitionDate.split('T')[0];
+          let demoStatus = data.data.geocodeAddress.edges[0].node.demosByParcelno.edges[0].node.status;
+
+          if (demoStatus === "Completed") {
+            return agent.add(`The demolition of ${address} was completed on ${demoDate}.`)
+          }
+          else {
+            return agent.add(`The demolition of ${address} is planned for ${demoDate}.`)
+          }
+      })
+      .catch(e => console.log(e));
+    }
+
+    let intentMap = new Map();
+    intentMap.set('Default Welcome Intent', welcome);
+    intentMap.set('Default Fallback Intent', fallback);
+    intentMap.set('trash', trash);
+    intentMap.set('permits.single', permitsSingle);
+    intentMap.set('permits.single - yes', permitsDetails);
+    intentMap.set('demolitions.single', demolitionSingle);
+    agent.handleRequest(intentMap);
+  });
